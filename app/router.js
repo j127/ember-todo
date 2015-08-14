8ab3b5b24920b85d26054ae64d402d8440e17d85
J. Cohen <hostelmarketing@gmail.com>
2015-08-14 13:14:32 -0700

    Refactor

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function () {
-    this.resource('todos', { path: '/' }, function(){
+    this.resource('todos', { path: '/' }, function () {
         this.route('active');
     });
     this.route('todos', function () {
