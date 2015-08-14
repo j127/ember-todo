aac0e419ef0ced8d4049715ae045ce0e96f2c255
J. Cohen <hostelmarketing@gmail.com>
2015-08-14 09:01:35 -0700

    Wire together things

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function () {
         return this.store.find('todo');
-    }
+    },
+    actions: {
+        createTodo: function (newTitle) {
+            // Create the new Todo model
+            var todo = this.store.createRecord('todo', {
+                title: newTitle,
+                isComplete: false
+            });
+ 
+            // Clear the "New Todo" text field
+            this.controllerFor('todos').set('newTitle', '');
+ 
+            todo.save();
+       }
+   }
 });
